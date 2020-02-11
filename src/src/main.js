e0537efd91f33d5da420723f935ec3769c1b7dc4
hssanchez17 <hssanchez.17@est.ucab.edu.ve>
2020-02-11 11:54:03 -0430

    Lista configuracion de vue

---
+++ src/src/main.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+
+import Vuelidate from 'vuelidate'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueCookie from 'vue-cookie';	
+
+
+Vue.use(VueAxios, axios)
+Vue.use(Vuelidate)
+Vue.use(VueCookie);
+
+
+// Agregamos la URL base de nuestra API
+axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.withCredentials = true
+
+Vue.use(BootstrapVue)
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
