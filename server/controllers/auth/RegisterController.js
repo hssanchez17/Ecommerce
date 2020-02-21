40b2150cd628a1dfd8f92d0dd880b7712c3c4d4b
hssanchez17 <hssanchez.17@est.ucab.edu.ve>
2020-02-21 16:17:00 -0430

    Using ES6

--- server/controllers/auth/RegisterController.js
+++ server/controllers/auth/RegisterController.js
@@ -1,15 +1,14 @@
 const {hashPassword}=require('../../config/password')
-const {database}=require('../../config/database')
+const {User}=require('../../models/User.js')
 
 const register= async(req,res)=>{
     let {name,lastName,email,phone,password,address}=req.body
     password= await hashPassword(password)
-    const text = 'INSERT INTO users(name, email,phone,password,address,lastName) VALUES ($1, $2, $3, $4, $5,$6)'
-    const values = [name, email,phone,password,address,lastName]
 
-    database.query(text, values)
-    .then(function(){res.status(200).json('registro Exitoso')})
-    .catch(e => console.error(e.stack))
+    const user= new User(name,lastName,email,phone,password,address,password)
+    console.log (await user.create())
+    //if(user.create())res.status(200).json('registro Exitoso')
+    //res.send({user:user})
 }
 
 module.exports={
---
+++ server/models/User.js
@@ -0,0 +1,31 @@
+'use strict';
+const {database}=require('../config/database')
+
+
+class User{
+	constructor(name,lastName,email,phone,password,address){
+		this.name=name;
+		this.lastName=lastName;
+		this.email=email;
+		this.phone=phone;
+		this.password=password;
+		this.address=address;
+	}
+
+	create(){
+
+    	return new Promise((resolve, reject) => {
+        	const text = 'INSERT INTO users(name, email,phone,password,address,lastName) VALUES ($1, $2, $3, $4, $5,$6)'
+    		const values = [this.name, this.email,this.phone,this.password,this.address,this.lastName]
+
+         	database.query(text, values)
+           .then(function(){resolve(true)})
+           .catch(e => console.error(e.stack))
+      
+          })
+        }
+	}
+
+
+
+module.exports={User}
